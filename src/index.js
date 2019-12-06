4f78789295932793791a907354e52e526d6e0205
Joon <joonseo.an@outlook.com>
2019-12-05 22:15:56 -0500

    grid basic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './Components/App';
 // For Pactice
 import GridPlayGround from './Grid_PlayGround/PlayGround';
 import Challenge from './Grid_PlayGround/Challenge';
+import ImplicitExplicitGrid from './Grid_PlayGround/ImplicitExplicitGrid';
 
-ReactDOM.render(<Challenge />, document.getElementById('root'));
+ReactDOM.render(<ImplicitExplicitGrid />, document.getElementById('root'));
 
